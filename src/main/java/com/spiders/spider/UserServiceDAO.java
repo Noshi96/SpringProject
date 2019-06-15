703b0309761d5e4d7076c7d76ef9532ff1f848c1
Noshi96 <2spawel2@gmail.com>
2019-06-15 16:48:10 +0200

    Initial commit

---
+++ src/main/java/com/spiders/spider/UserServiceDAO.java
@@ -0,0 +1,104 @@
+package com.spiders.spider;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.ConnectionCallback;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class UserServiceDAO {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Users> getUsers(){
+        List<Users> userList = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+        Collection<Map<String, Object>> rows = jdbcTemplate.queryForList(sql);
+
+        rows.stream().map((row)->{
+            Users users = new Users();
+            users.setId((Integer) row.get("id"));
+            users.setName((String) row.get("name"));
+            users.setRole((String) row.get("role"));
+            return users;
+        }).forEach((user)->{
+
+            userList.add(user);
+        });
+        return userList;
+    }
+
+    public void createUser(Users user) {
+        jdbcTemplate.update((Connection connection)->{
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (name,role) VALUES (?,?)");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getRole());
+            return preparedStatement;
+        });
+    }
+
+    public void updateUser(Users user, int id) {
+        jdbcTemplate.update((Connection connection)->{
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE users SET name=?, role=? WHERE id="+ id +"");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getRole());
+            return preparedStatement;
+        });
+    }
+
+    public void deleteUser(int id) {
+        jdbcTemplate.update((Connection connection)->{
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users WHERE id="+ id +"");
+            return preparedStatement;
+        });
+    }
+
+    public List<Users> getById(int id){
+        List<Users> userList = new ArrayList<>();
+        String sql = "SELECT * FROM users WHERE id='"+ id +"'";
+        Collection<Map<String, Object>> rows = jdbcTemplate.queryForList(sql);
+
+        rows.stream().map((row)->{
+            Users users = new Users();
+            users.setId((Integer) row.get("id"));
+            users.setName((String) row.get("name"));
+            users.setRole((String) row.get("role"));
+            return users;
+        }).forEach((user)->{
+
+            userList.add(user);
+        });
+        return userList;
+    }
+
+    public List<Users> sqlQuery(String sql){
+        List<Users> userList = new ArrayList<>();
+        Collection<Map<String, Object>> rows = jdbcTemplate.queryForList(sql);
+
+        rows.stream().map((row)->{
+            Users users = new Users();
+            users.setId((Integer) row.get("id"));
+            users.setName((String) row.get("name"));
+            users.setRole((String) row.get("role"));
+            return users;
+        }).forEach((user)->{
+
+            userList.add(user);
+        });
+        return userList;
+    }
+
+    public String getByName(String name){
+        return "SELECT * FROM users WHERE name='"+ name +"'";
+    }
+
+}
---
+++ src/main/java/com/spiders/spider/UserServiceRestController.java
@@ -0,0 +1,112 @@
+package com.spiders.spider;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserServiceRestController {
+
+    @Autowired
+    UserServiceDAO userServiceDAO;
+
+    /**
+     * Metoda wyświetla listę wszsytkich użytkowników w bazie.
+     * Metoda : [GET]
+     * URL    : /users
+     * SQL    : SELECT
+     * TABALA : users
+     *
+     * @return
+     */
+    @RequestMapping(value="/users", method=RequestMethod.GET)
+    public List<Users> getUsers(){
+        return userServiceDAO.getUsers();
+    }
+
+    /**
+     * Metoda pozwala na wprowadzenie nowego użytkownika.
+     * Metoda : [POST]
+     * URL    : /users
+     * SQL    : INSERT
+     * Tabela : users
+     *
+     * @param user Podany nowy użytkownik, który dostanie role oraz name.
+     * @return Informację że dodano poprawnie użytkownika.
+     */
+    @RequestMapping(value="/users", method=RequestMethod.POST) // zmien na POST
+    public String insertUser(@RequestBody Users user){//walidacja
+        userServiceDAO.createUser(user);
+        return "User created successfully";
+    }
+
+    /**
+     * Metoda pozwala na aktualizację podanego użytkownika o podanym w parametrze url identyfikatorze.
+     * Metoda : [POST]
+     * URL    : /users/{id}
+     * SQL    : UPDATE
+     * Tabela : users
+     *
+     * @param user Podany użytkownik, który będzie aktualizowany.
+     * @param id Identyfikator podawany w adresie url.
+     * @return Informację, że zaktualizowano poprawnie użytkownika.
+     */
+    @RequestMapping(value="/users/{id}", method=RequestMethod.POST)
+    public String updateUser(@RequestBody Users user, @PathVariable int id){
+        userServiceDAO.updateUser(user, id);
+        return "User updated successfully";
+    }
+
+    /**
+     * Metoda pozwala na usunięcie podanego użytkownika o podanym w parametrze url identyfikatorze.
+     * Metoda : [POST]
+     * URL    : /delUser/{id}
+     * SQL    : DELETE
+     * Tabela : users
+     *
+     * @param id Identyfikator podawany w adresie url.
+     * @return Informację, że usunięto poprawnie użytkownika.
+     */
+    @RequestMapping(value = "/delUser/{id}", method = RequestMethod.POST)
+    public String addUser(@PathVariable int id){
+        userServiceDAO.deleteUser(id);
+        return "User deleted successfully";
+    }
+
+    /**
+     * Metoda pozwala na znalezienie i pokazanie użytkownika o podanym w url identyfikatorze.
+     * Metoda : [GET]
+     * URL    : /getById/{id}
+     * SQL    : SELECT
+     * Tabela : users
+     *
+     * @param id Identyfikator podawany w adresie url.
+     * @return
+     */
+    @RequestMapping(value = "/getById/{id}", method = RequestMethod.GET)
+    public List<Users> getById(@PathVariable int id){
+        return userServiceDAO.getById(id);
+    }
+
+
+    /**
+     * Metoda pozwala na znalezienie i pokazanie użytkownika o podanym w url imieniu.
+     *      Wykorzystywana jest tutaj metoda sqlQuery(), która pozwala tworzyć zapytania SQL.
+     *      Metoda getByName() tworzy zapytanie SQL o parametrze WHERE=name.
+     * Metoda : [GET]
+     * URL    : /getByName/{name}
+     * SQL    : SELECT
+     * Tabela : users
+     *
+     * @param name imię użytkownika podawane w adresie url.
+     * @return
+     */
+    @RequestMapping(value = "/getByName/{name}", method = RequestMethod.GET)
+    public List<Users> getByName(@PathVariable String name){
+        return userServiceDAO.sqlQuery(userServiceDAO.getByName(name));
+    }
+}
